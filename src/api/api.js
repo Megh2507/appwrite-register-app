2057699a64d85efa0e6dfaca6f5db1690bfdaeaa
Megha Paul <megha25072001@gmail.com>
2022-05-11 22:52:00 +0530

    changing api endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import { Appwrite } from 'appwrite';
 const appwrite = new Appwrite();
 
 appwrite
-    .setEndpoint('http://64.227.169.218/v1') // Your Appwrite Endpoint
+    .setEndpoint('https://www.megha25.me/v1') // Your Appwrite Endpoint
     .setProject('626837229e51100ea1f0') // Your project ID
 ;
 export default appwrite;
